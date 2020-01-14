306d110cebe053f63d8f38ea2f306d9c28bea40b
vikrant <vikrant@dell.local>
2020-01-14 15:59:29 +0530

    updated

---
+++ Anuja/TFIDF_word-Embedding.py
@@ -0,0 +1,12 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+corpus = [
+    'This is the first document.',
+    'This document is the second document.',
+    'And this is the third one.',
+    'Is this the first document?',
+]
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(corpus)
+print(vectorizer.get_feature_names())
+
+print(X)
