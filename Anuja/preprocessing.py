e2892d1b6e2843b7b6420a7c9bf29b732d2dd255
vikrant <vikrant@dell.local>
2020-02-13 13:15:37 +0530

    first simple cycle

--- Anuja/preprocessing.py
+++ Anuja/preprocessing.py
@@ -1,18 +1,21 @@
+# import gensim
+# from gensim.models import Word2Vec
 import pandas as pd
 import os
 import string
 import numpy as np
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-# import gensim
-# from gensim.models import Word2Vec
+from sklearn.model_selection import StratifiedKFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
 
 def dataLoad():
-    path="/home/vikrant/github/study/Anuja"
-    return pd.read_csv(os.path.join(os.getcwd(),"Anuja", "data","train_data.csv"), encoding='utf-8')
+    # path="/home/vikrant/github/study/Anuja"
+    return pd.read_csv(os.path.join(os.getcwd(), "data","train_data.csv"), encoding='utf-8')
     # return pd.read_csv(os.path.join(path, "data","train_data.csv"), encoding='utf-8')
 
 #remove Punctuation and Tokenising the review
@@ -32,7 +35,7 @@ def trainTestSplit(df):
     y=df.iloc[:,2]
     return train_test_split(X, y, test_size=0.33, random_state=42)
 
-#convert the reviews into word matrix (find the inportant word and plot them as feature )
+#convert the reviews into word matrix (find the important word and plot them as feature )
 def Word2VecTFIDF(Corpus):
     vectorizer = TfidfVectorizer(min_df=1,stop_words='english')
     X = vectorizer.fit_transform(Corpus)
@@ -46,12 +49,15 @@ def NaiveBayes(X_train,y_train):
 
 def main():
     # print("vvv")
+    # print(os.getcwd())
+
     df=dataLoad()
     df=tokenising(df)
     LabelEncoding(df)
+
     X_train, X_test, y_train, y_test=trainTestSplit(df)
 
-    vectorizer = TfidfVectorizer(min_df=1,stop_words='english')
+    vectorizer = TfidfVectorizer(min_df=1,stop_words="english")
     X_trainNew=vectorizer.fit_transform(X_train)
     X_testNew=vectorizer.transform(X_test)
 
