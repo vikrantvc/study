fc1a70354cd0a1df44e77db691a3cdf65d1d87e2
vikrant <vikrant@dell.local>
2020-02-13 13:13:15 +0530

    no change

--- Anuja/test.py
+++ Anuja/test.py
@@ -3,6 +3,15 @@ import os
 from sklearn.model_selection import train_test_split
 
 df = pd.DataFrame({'text':['a..b?!??', '%hgh&12','abc123!!!', '$$$1234']})
-print(df)
+# print(df)
 df['text'] = df['text'].str.replace(r'[^\w\s]+', '')
-print(df)
+# print(df)
+
+
+
+x=6.666
+x=str(x)
+x=x[:4]
+
+x=float(x)
+print(type(x))
